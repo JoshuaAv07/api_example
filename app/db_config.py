12cc428975ed52fe9cf8dc7c6c1e86d88325997b
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-03-24 09:14:39 -0700

    comments

--- app/db_config.py
+++ app/db_config.py
@@ -1,10 +1,13 @@
 from flask_pymongo import pymongo
 import os
 
-DB_USER = os.getenv("DB_USER")
+#next three lines brings the data from .env
+DB_USER = os.getenv("DB_USER") 
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
+#connects to the mongo's database that ir created for the project
 client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@api-example.bx3ji.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
 
+#declares the database connection as variable
 db = client.students
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app.main import app
 
+#runs the app using gunicorn server by using main.py
 if __name__ == '__main__':
-    app.run(load_dotenv=True)
+    app.run(load_dotenv=True) 
